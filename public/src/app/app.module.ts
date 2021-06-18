68d8dfa96f41cd4e73df7871a574c01f2e059e2e
Jan Tache <j.andreitabs@gmail.com>
2021-06-18 01:58:55 -0700

    Add default messages component

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ResourcesComponent } from './resources/resources.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ResourcesComponent
+    ResourcesComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
