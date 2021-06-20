6a22706bb33f1d15bb754a5406b403a41eebccb5
Jan Tache <j.andreitabs@gmail.com>
2021-06-20 01:48:15 -0700

    Clean up resource service

--- public/src/app/resource.service.ts
+++ public/src/app/resource.service.ts
@@ -1,11 +1,9 @@
 import { Injectable, isDevMode } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 import { Resource } from './resource';
-import { RESOURCES } from './mock-resources';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
@@ -16,18 +14,11 @@ export class ResourceService {
   apiURL: string = '';
 
   constructor(
-    private messageService: MessageService,
     private http: HttpClient,
   ) {
     if (isDevMode()) {
       this.apiURL = 'http://localhost:8000';
-    } else {
-      this.apiURL = '';
     }
-  }
-
-  private log(msg: string) {
-    this.messageService.add(`ResourceService: ${msg}`);
   }
 
   private resourceURL(): string {
@@ -40,6 +31,10 @@ export class ResourceService {
 
   getResource(name: string): Observable<Resource | undefined> {
     return this.http.get<Resource[]>(this.resourceURL())
-      .pipe(map((resources) => resources.find((res) => res.name === name)));
+      .pipe(
+        map((resources) => resources.find(
+          (res) => res.name === name
+        ))
+      );
   }
 }
